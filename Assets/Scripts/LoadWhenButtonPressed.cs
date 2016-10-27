2042ff18b8ff551d99df40fa5e54836d8a895cb9
Bradley Trowbridge <bradley.trowbridge@yahoo.com>
2016-10-26 23:54:24 -0400

    Aplying .gitignore

---
+++ Assets/Scripts/LoadWhenButtonPressed.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadWhenButtonPressed : MonoBehaviour {
+
+	//needs to be accessible and returns nothing
+
+	public void LoadScene(int MenuOrGame)
+	{
+		Application.LoadLevel (MenuOrGame);
+	}
+}
